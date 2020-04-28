5a7307ece99b173342ebd843b3a1873bfb71ca91
Mitchell Skaggs <skaggsm333@gmail.com>
2020-04-28 18:48:09 -0500

    Update to use new currentVelocityX/Y fields for motion blur

--- library/src/main/java/com/github/matteobattilana/weather/confetti/WeatherConfettoGenerator.kt
+++ library/src/main/java/com/github/matteobattilana/weather/confetti/WeatherConfettoGenerator.kt
@@ -16,21 +16,12 @@ class WeatherConfettoGenerator(val confettoInfo: ConfettoInfo) : ConfettoGenerat
 class MotionBlurBitmapConfetto(val confettoInfo: ConfettoInfo) : Confetto() {
     companion object {
         const val SNOW_RADIUS = 7.5f
-        const val RAIN_STRETCH = 1.5f
+        const val RAIN_STRETCH = 20f
     }
-
-    private var prevX: Float? = null
-    private var prevY: Float? = null
 
     override fun getHeight(): Int = 0
 
     override fun getWidth(): Int = 0
-
-    override fun reset() {
-        super.reset()
-        prevX = null
-        prevY = null
-    }
 
     override fun configurePaint(paint: Paint) {
         super.configurePaint(paint)
@@ -39,19 +30,15 @@ class MotionBlurBitmapConfetto(val confettoInfo: ConfettoInfo) : Confetto() {
     }
 
     override fun drawInternal(canvas: Canvas, matrix: Matrix, paint: Paint, x: Float, y: Float, rotation: Float, percentageAnimated: Float) {
-        if (prevX == null || prevY == null) {
-            prevX = x
-            prevY = y
-        }
 
         when (confettoInfo.precipType) {
                 PrecipType.CLEAR -> {
                 }
                 PrecipType.RAIN -> {
-                    val dX = x - prevX!!
-                    val dY = y - prevY!!
-                    val x1 = prevX!! - dX * RAIN_STRETCH
-                    val y1 = prevY!! - dY * RAIN_STRETCH
+                    val dX = currentVelocityX
+                    val dY = currentVelocityY
+                    val x1 = x - dX * RAIN_STRETCH
+                    val y1 = y - dY * RAIN_STRETCH
                     val x2 = x + dX * RAIN_STRETCH
                     val y2 = y + dY * RAIN_STRETCH
 
@@ -71,8 +58,5 @@ class MotionBlurBitmapConfetto(val confettoInfo: ConfettoInfo) : Confetto() {
                     canvas.drawCircle(x, y, SNOW_RADIUS, paint)
                 }
         }
-
-        prevX = x
-        prevY = y
     }
 }
